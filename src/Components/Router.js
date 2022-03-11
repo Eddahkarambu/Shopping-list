7a68c142c711917ea3f29d700d7beb3965779c29
Eddah Karambu <gikundaeddah@gmail.com>
2022-03-11 14:09:40 +0300

    install prettier and eslint

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -2,13 +2,12 @@ import React, { Component } from "react";
 import { Routes, Route } from "react-router-dom";
 import Signup from "./Signup";
 
-
 export default class Paths extends Component {
-    render() {
-        return (
-                <Routes>
-                    <Route path="/" element={<Signup />}/>
-                </Routes>
-        )
-    }
+  render() {
+    return (
+      <Routes>
+        <Route path="/" element={<Signup />} />
+      </Routes>
+    );
+  }
 }
