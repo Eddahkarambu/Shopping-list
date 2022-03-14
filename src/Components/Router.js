e1909f6ad64fdfff40e80545ee9e6e69d0496e7f
Eddah Karambu <gikundaeddah@gmail.com>
2022-03-14 17:40:20 +0300

    create signin page, design and style it

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import { Routes, Route } from "react-router-dom";
 import Signup from "./Signup";
+import Signin from "./Signin";
 
 export default class Paths extends Component {
   render() {
     return (
       <Routes>
         <Route path="/" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
       </Routes>
     );
   }
