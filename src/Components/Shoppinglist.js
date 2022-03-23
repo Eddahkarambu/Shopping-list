e50bb838be2c093509cdd7d4afd7b0ec861e108c
Eddah Karambu <gikundaeddah@gmail.com>
2022-03-23 13:31:02 +0300

    add modal and button

--- src/Components/Shoppinglist.js
+++ src/Components/Shoppinglist.js
@@ -1,6 +1,22 @@
 import React, { useState, useEffect } from "react";
 import "./Shoppinglist.css";
 import { DataGrid } from "@mui/x-data-grid";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+};
 
 const columns = [
   { field: "id", headername: "ID" },
@@ -8,6 +24,9 @@ const columns = [
 ];
 
 export default function Shoppinglist() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   const [shoppinglist, setShoppingList] = useState([]);
 
   useEffect(() => {
@@ -45,6 +64,23 @@ export default function Shoppinglist() {
   };
   return (
     <div className="shopping">
+      <Button onClick={handleOpen}>Add Shopping List</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      ></Modal>
+
+      <Box sx={style}>
+        <Typography id="modal-modal-title" variant="h6" component="h2">
+          Create Shopping List
+        </Typography>
+        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+          Name:
+        </Typography>
+      </Box>
+
       <div className="list">SHOPPING LIST</div>
       <div style={{ height: 400, width: "100%" }}>
         <DataGrid
