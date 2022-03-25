da8839617db9171e8a8e5af384044b6384b680f6
Eddah Karambu <gikundaeddah@gmail.com>
2022-03-25 17:02:09 +0300

    add handlesubmit function,used toast to show success and error,style button

--- src/Components/Shoppinglist.js
+++ src/Components/Shoppinglist.js
@@ -2,9 +2,23 @@ import React, { useState, useEffect } from "react";
 import "./Shoppinglist.css";
 import { DataGrid } from "@mui/x-data-grid";
 import Button from "@mui/material/Button";
-import Typography from "@mui/material/Typography";
 import Modal from "@mui/material/Modal";
+import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import { useNavigate } from "react-router-dom";
+import { makeStyles } from "@material-ui/core/styles";
+import { TextField, InputAdornment } from "@mui/material";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& .MuiFilledInput-root": {
+      background: "white",
+    },
+    width: "100%",
+  },
+}));
 
 const style = {
   position: "absolute",
@@ -24,10 +38,16 @@ const columns = [
 ];
 
 export default function Shoppinglist() {
-  const [open, setOpen] = React.useState(false);
+  let navigate = useNavigate();
+  const classes = useStyles();
+  const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
   const [shoppinglist, setShoppingList] = useState([]);
+  const [name, setName] = useState("");
+  const [error, setError] = useState(false);
+
+  console.log(open);
 
   useEffect(() => {
     fetchShoppingList();
@@ -44,6 +64,7 @@ export default function Shoppinglist() {
           Authorization: `Bearer ${token}`,
         },
       });
+
       let resJson = await res.json();
       if (res.status === 200) {
         const shoppingArray = resJson.data;
@@ -53,7 +74,6 @@ export default function Shoppinglist() {
             Name: list.attributes.Name,
           };
         });
-
         setShoppingList(modifieddArray);
       } else {
         alert("Some error occured");
@@ -62,25 +82,82 @@ export default function Shoppinglist() {
       alert(err);
     }
   };
+
+  let handleSubmit = async (e) => {
+    e.preventDefault();
+    if (name === "") {
+      alert("please fill the name");
+    }
+    try {
+      const token = localStorage.getItem("token");
+      let res = await fetch("http://localhost:1337/api/shoppinglists", {
+        method: "POST",
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify({
+          data: { Name: name },
+        }),
+      });
+      let resJson = await res.json();
+      if (res.status === 200) {
+      }
+      toast.success("successfully added your shoppinglist");
+      fetchShoppingList();
+      handleClose();
+      setName("");
+    } catch (err) {
+      toast.error("err");
+    }
+  };
   return (
     <div className="shopping">
+      <ToastContainer />
       <Button onClick={handleOpen}>Add Shopping List</Button>
       <Modal
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
-      ></Modal>
-
-      <Box sx={style}>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          Create Shopping List
-        </Typography>
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          Name:
-        </Typography>
-      </Box>
-
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            Create Shopping List
+          </Typography>
+          <form>
+            <Box margin="2rem">
+              <TextField
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start"></InputAdornment>
+                  ),
+                }}
+                placeholder="Name"
+                variant="filled"
+                className={classes.root}
+                type="Name"
+                required
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
+            </Box>
+          </form>
+          <div className="btn">
+            <Button
+              style={{
+                minWidth: "150px",
+              }}
+              variant="contained"
+              color="primary"
+              onClick={handleSubmit}
+            >
+              SUBMIT
+            </Button>
+          </div>
+        </Box>
+      </Modal>
       <div className="list">SHOPPING LIST</div>
       <div style={{ height: 400, width: "100%" }}>
         <DataGrid
